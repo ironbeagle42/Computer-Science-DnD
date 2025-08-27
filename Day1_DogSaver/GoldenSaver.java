ea8a0b44989b5d154c04020efb8537d594b0ed91
ironbeagle42 <mmaynard1@hwemail.com>
2025-08-27 13:31:54 -0700

    end of class aug 27

--- Day1_DogSaver/GoldenSaver.java
+++ Day1_DogSaver/GoldenSaver.java
@@ -6,10 +6,19 @@ public class GoldenSaver {
 	//Moves every dog whose breed is "Pug" in the list to the back of the list
 	public static void rescueGoldens(ArrayList<Dog> list) {
 		for (int i = 0; i < list.size(); i++) {
-			if (!list.get(i).getBreed().contains("Golden")) {
-				Dog temp = list.get(i);
-				list.remove(i);
-				list.add(0, temp);
+			if (list.get(i).getBreed().contains("Golden")) {
+				for (int j = list.size() - 1; j >= 0; j--) {
+					if (!list.get(j).getBreed().contains("Golden")) {
+						if (j < i) {
+							i=list.size();
+							break;
+						}
+						Dog temp = list.get(j);
+						list.set(j, list.get(i));
+						list.set(i, temp);
+						j = -1;
+					}
+				}
 			}
 		}
 
--- Day1_DogSaver/GoldenSaverTester.java
+++ Day1_DogSaver/GoldenSaverTester.java
@@ -1,6 +1,15 @@
 import java.util.ArrayList;
 public class GoldenSaverTester {
     public static void main(String[] args) {
+        Dog dog1 = new Dog("name", "Golden Retriever");
+        Dog dog2 = new Dog("name", "Pug");
         ArrayList<Dog> list = new ArrayList<Dog>();
+        for (int i = 0; i < 5; i++) {
+            list.add(dog1);
+            list.add(dog2);
+        }
+        System.out.println(list);
+        GoldenSaver.rescueGoldens(list);
+        System.out.println(list);
     }
 }
