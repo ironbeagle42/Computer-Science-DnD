ab9a871ecf2d7b8194d150ee28f250ed29799fe7
ironbeagle42 <mmaynard1@hwemail.com>
2026-01-27 13:32:34 -0800

    1/27

---
+++ Unit4_CookieMonster/CookieMonster.java
@@ -0,0 +1,111 @@
+import java.io.*;
+import java.util.*;
+import java.util.Stack;
+import com.apple.laf.resources.aqua_zh_CN;
+import java.util.Queue;
+
+// You are allowed (and expected!) to use either Java's ArrayDeque or LinkedList class to make
+// stacks and queues
+
+
+public class CookieMonster {
+
+	private int[][] cookieGrid;
+	private int numRows;
+	private int numCols;
+
+	// Constructs a CookieMonster from a file with format:
+	// numRows numCols
+	// <<rest of the grid, with spaces in between the numbers>>
+	public CookieMonster(String fileName) {
+		int row = 0;
+		int col = 0;
+		try {
+			Scanner input = new Scanner(new File(fileName));
+
+			numRows = input.nextInt();
+			numCols = input.nextInt();
+			cookieGrid = new int[numRows][numCols];
+
+			for (row = 0; row < numRows; row++)
+				for (col = 0; col < numCols; col++)
+					cookieGrid[row][col] = input.nextInt();
+
+			input.close();
+		} catch (Exception e) {
+			System.out.print("Error creating maze: " + e.toString());
+			System.out.println("Error occurred at row: " + row + ", col: " + col);
+		}
+
+	}
+
+	public CookieMonster(int[][] cookieGrid) {
+		this.cookieGrid = cookieGrid;
+		this.numRows = cookieGrid.length;
+		this.numCols = cookieGrid[0].length;
+	}
+
+	// You may find it VERY helpful to write this helper method. Or not!
+	private boolean validPoint(int row, int col) {
+		if (cookieGrid[row][col] == -1) {
+			return false;
+		}
+		return true;
+	}
+
+	/*
+	 * RECURSIVELY calculates the route which grants the most cookies. Returns the maximum number of
+	 * cookies attainable.
+	 */
+	public int recursiveCookies() {
+		return recursiveCookies(0, 0);
+	}
+
+	// Returns the maximum number of cookies edible starting from (and including)
+	// cookieGrid[row][col]
+	public int recursiveCookies(int row, int col) {
+		if (!validPoint(row, col)) {
+			return -1;
+		}
+		int numCookies = 0;
+		if (row != cookieGrid.length - 1) {
+			numCookies = recursiveCookies(row + 1, col) + cookieGrid[row][col];
+			if (numCookies < cookieGrid[row][col]) {
+				numCookies = -1;
+			}
+		}
+		if (col != cookieGrid[0].length - 1) {
+			if (recursiveCookies(row, col + 1) > numCookies) {
+				numCookies = recursiveCookies(row, col + 1) + cookieGrid[row][col];
+			}
+			if (numCookies < cookieGrid[row][col]) {
+				return -1;
+			}
+		}
+
+		return numCookies;
+	}
+
+
+	/*
+	 * Calculate which route grants the most cookies using a QUEUE. Returns the maximum number of
+	 * cookies attainable.
+	 */
+	/* From any given position, always add the path right before adding the path down */
+	public int queueCookies() {
+		// CODE THIS
+		return 0;
+	}
+
+
+	/*
+	 * Calculate which route grants the most cookies using a stack. Returns the maximum number of
+	 * cookies attainable.
+	 */
+	/* From any given position, always add the path right before adding the path down */
+	public int stackCookies() {
+		// CODE THIS
+		return 0;
+	}
+
+}
