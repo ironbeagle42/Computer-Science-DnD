f1a2ade071f78e1bd37ebc2be09e6a105b8b969c
ironbeagle42 <mmaynard1@hwemail.com>
2026-03-13 12:20:31 -0700

    idk

--- Unit6_MiniGPT/MiniGPTTester.java
+++ Unit6_MiniGPT/MiniGPTTester.java
@@ -1,6 +1,6 @@
 public class MiniGPTTester {
     public static void main(String[] args) {
         MiniGPT tester = new MiniGPT("thegreatgatsby.txt", 10);
-        tester.generateText("greatGatsby2.txt", 1000);
+        tester.generateText("greatGatsby2.txt", 2000);
     }
 }
