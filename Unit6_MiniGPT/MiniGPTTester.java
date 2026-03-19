73a7afb6a7c3ef13bcb7a4d3defe284709123ee3
ironbeagle42 <mmaynard1@hwemail.com>
2026-03-19 09:05:43 -0700

    lawsuit

--- Unit6_MiniGPT/MiniGPTTester.java
+++ Unit6_MiniGPT/MiniGPTTester.java
@@ -1,6 +1,6 @@
 public class MiniGPTTester {
     public static void main(String[] args) {
         MiniGPT tester = new MiniGPT("math.txt", 8);
-        tester.generateText("math2.txt", 10000);
+        tester.generateText("lawsuit.txt", 10000);
     }
 }
